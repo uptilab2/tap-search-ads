fc67388a79783b06b6d02f92d4dead4de4e6bcbb
ngteric <nongeric@gmail.com>
2020-03-04 11:57:17 +0100

    wip

--- tap_searchads360/streams.py
+++ tap_searchads360/streams.py
@@ -42,14 +42,22 @@ class SearchAdsStream(Stream):
     valid_replication_keys = ['lastModifiedTimestamp']
     replication_key = 'lastModifiedTimestamp'
 
-    def __init__(self, name, config=None, **kwargs):
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
         self.key_properties = [name+'Id']
-        if 'metrics_date_segment' in config:
-            if config['metrics_date_segment']:
+        # setting up optional config
+        if 'metrics_date_segment' in self.config:
+            if self.config['metrics_date_segment']:
                 self.replication_key = 'date'
                 self.valid_replication_keys = ['date']
 
-        super().__init__(name, config=config, **kwargs)
+    def selected_properties(self, metadata):
+        # add selected false to properties we don't want
+        print(metadata)
+        mdata, columns = [(field, field['breadcrumb'][1]) for field in metadata if 'date' not in field['breadcrumb']]
+        print(mdata)
+        mdata.append({'breadcrumb':['properties', 'date'], 'metadata': {'inclusion': 'available', 'selected':'false'}})
+        return columns, mdata
 
     def write(self, metadata):
         self.write_schema()
@@ -57,11 +65,12 @@ class SearchAdsStream(Stream):
         self.write_state()
         logger.info(f'Finished sync stream: {self.name}')
 
-    def request_data(self, schema):
+    def request_data(self, columns):
         yesterday = datetime.now() - timedelta(days=1)
         default_end_date = yesterday.strftime('%Y-%m-%d')
         if self.config['start_date'][:10] > str(yesterday) and 'end_date' not in self.config:
             raise DateRangeError(f"start_date should be at least 1 days ago")
+
         payloads = {
             'reportScope':{
                 'agencyId': self.config['agency_id'],
@@ -69,7 +78,7 @@ class SearchAdsStream(Stream):
                 'engineAccountId': self.config['engineAccount_id']
             },
             'reportType': self.name,
-            'columns': [{'columnName': column_name} for column_name in schema['properties']],
+            'columns': [{'columnName': column_name} for column_name in columns],
             'timeRange': {
                 'startDate': self.config['start_date'][:10],
                 'endDate': default_end_date
@@ -91,7 +100,8 @@ class SearchAdsStream(Stream):
     def sync(self, mdata):
         logger.info(f'syncing {self.name}')
         schema = self.load_schema()
-        data = self.client.get_data(self.request_data(schema))
+        columns, metadata = self.selected_properties(mdata)
+        data = self.client.get_data(self.request_data(columns))
         if data:
             bookmark = self.get_bookmark()
             new_bookmark = bookmark
@@ -99,7 +109,7 @@ class SearchAdsStream(Stream):
                 with singer.metrics.record_counter(endpoint=self.name) as counter:
                     for d in data:
                         with singer.Transformer(integer_datetime_fmt="unix-seconds-integer-datetime-parsing") as transformer:
-                            transformed_record = transformer.transform(data=d, schema=schema, metadata=singer.metadata.to_map(mdata))
+                            transformed_record = transformer.transform(data=d, schema=schema, metadata=singer.metadata.to_map(metadata))
                             new_bookmark = max(new_bookmark, transformed_record.get(self.replication_key))
                             if (self.replication_method == 'INCREMENTAL' and transformed_record.get(self.replication_key) > bookmark) or self.replication_method == 'FULL_TABLE':
                                 singer.write_record(stream_name=self.name, time_extracted=singer.utils.now(), record=transformed_record)
